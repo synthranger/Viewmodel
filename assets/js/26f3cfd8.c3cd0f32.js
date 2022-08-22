743d494fbae0d1e450761e0a5aa365acd4066d8a
Synthranger <synthrangerofficial@gmail.com>
2022-08-22 13:22:00 +0800

    Built and published by Moonwave

---
+++ assets/js/26f3cfd8.c3cd0f32.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[45],{82824:function(e){e.exports=JSON.parse('[{"type":"link","href":"/api/Viewmodel","label":"\u200bViewmodel"}]')}}]);
---
+++ assets/js/a812d3c8.f84bf9e4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[992],{97868:function(e){e.exports=JSON.parse('{"sourceUrl":"https://github.com/Synthranger/Viewmodel/blob/main","baseUrl":"/Viewmodel/","classOrder":[],"apiCategories":[]}')}}]);
